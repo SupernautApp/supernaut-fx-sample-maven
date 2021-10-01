988da04f00fc2fea72c2a558c3d98a2fb2038864
Sean Gilligan <sean@msgilligan.com>
2021-10-01 14:14:25 -0700

    Clean up disabled Hyperlink generation code
    * Update link URI to https://www.supernaut.app
    
    * Use `javafx.runtime.version` rather than `javafx.version` (it is more detailed)
    
    * Move “hello” string generation to a private method
    
    * Create a boolean flag to prepare for a future JavaFX where it will work in native mode on macOS, see:
    https://github.com/SupernautApp/SupernautFX/issues/25

--- src/main/java/app/supernaut/fx/sample/maven/HelloFX.java
+++ src/main/java/app/supernaut/fx/sample/maven/HelloFX.java
@@ -4,6 +4,7 @@ import app.supernaut.fx.FxForegroundApp;
 import app.supernaut.fx.FxLauncher;
 import app.supernaut.services.BrowserService;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
@@ -11,11 +12,12 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import jakarta.inject.Singleton;
+
 import java.net.URI;
 
 @Singleton
 public class HelloFX implements FxForegroundApp.FxApplicationCompat {
-    private static final URI githubRepoUri = URI.create("https://github.com/SupernautApp/SupernautFX");
+    private static final URI projectWebSiteUri = URI.create("https://www.supernaut.app");
     private final BrowserService browserService;
 
     public HelloFX(BrowserService browserService) {
@@ -30,14 +32,24 @@ public class HelloFX implements FxForegroundApp.FxApplicationCompat {
     }
 
     private Scene buildScene() {
+        var label       = new Label(buildHello());
+        var hyperlink   = new Hyperlink("Powered by Supernaut.FX");
+        // Hyperlinks don't work on macOS in Graal native-image yet, see https://github.com/SupernautApp/SupernautFX/issues/25
+        // TODO: Add a test that will set the below boolean to true if JavaFX > forthcoming 18-ea+4
+        boolean addHyperlink = false;
+        var vboxNodes = addHyperlink ? new Node[]{label, hyperlink} : new Node[]{label};
+        var vbox        = new VBox(vboxNodes);
+        vbox.setAlignment(Pos.CENTER);
+        hyperlink.setOnAction(e -> browserService.showDocument(projectWebSiteUri));
+        return new Scene(vbox, 350, 100);
+    }
+
+    private String buildHello() {
         var javaVersion = System.getProperty("java.version");
         var javafxVersion = System.getProperty("javafx.version");
-        var label       = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        //var hyperlink   = new Hyperlink("Powered by Supernaut.FX");  // Doesn't work in Graal native-image yet
-        var vbox        = new VBox(label /*, hyperlink */);
-        vbox.setAlignment(Pos.CENTER);
-        //hyperlink.setOnAction(e -> browserService.showDocument(githubRepoUri));
-        return new Scene(vbox, 350, 100);
+        var javaFxRuntimeVersion = System.getProperty("javafx.runtime.version");
+        return String.format("Hello, JavaFX  %s running on Java %s",
+                javaFxRuntimeVersion, javaVersion);
     }
 
     public static void main(String[] args) {
