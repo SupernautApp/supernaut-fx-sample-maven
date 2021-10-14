23917039a502d375822371d472b81f7ca505bf64
Sean Gilligan <sean@msgilligan.com>
2021-10-14 11:06:00 -0700

    Enable hyperlinks (unconditionally)
    Given that we can separately configure the OpenJFX version for the
    GluonFX build, this is the simplest thing to do.

--- src/main/java/app/supernaut/fx/sample/maven/HelloFX.java
+++ src/main/java/app/supernaut/fx/sample/maven/HelloFX.java
@@ -1,11 +1,9 @@
 package app.supernaut.fx.sample.maven;
 
 import app.supernaut.fx.ApplicationDelegate;
-import app.supernaut.fx.FxForegroundApp;
 import app.supernaut.fx.FxLauncher;
 import app.supernaut.services.BrowserService;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
@@ -17,7 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URI;
-import java.util.Locale;
 
 @Singleton
 public class HelloFX implements ApplicationDelegate {
@@ -41,32 +38,17 @@ public class HelloFX implements ApplicationDelegate {
     private Scene buildScene() {
         var label       = new Label(buildHello());
         var hyperlink   = new Hyperlink("Powered by Supernaut.FX");
-        // TODO: Add a test that will set the below boolean to true if JavaFX > forthcoming 18-ea+4
-        // See WIP in Supernaut.FX main repo
-        boolean addHyperlink = isShowDocumentAvailable();
-        var vboxNodes = addHyperlink ? new Node[]{label, hyperlink} : new Node[]{label};
-        var vbox        = new VBox(vboxNodes);
-        vbox.setAlignment(Pos.CENTER);
         hyperlink.setOnAction(e -> browserService.showDocument(projectWebSiteUri));
+        var vbox        = new VBox(label, hyperlink);
+        vbox.setAlignment(Pos.CENTER);
         return new Scene(vbox, 500, 100);
-    }
-
-    // We're working on a more proper version of this is the main Supernaut.FX repository.
-    // This will disable hyperlinks on macOS even on JVM when it only fails on GraalVM native-image
-    boolean isShowDocumentAvailable() {
-        return !isMacOS();
-    }
-
-    private boolean isMacOS() {
-        boolean isMacOS = System.getProperty("os.name", "").toLowerCase(Locale.US).contains("mac");
-        log.info("isMacOS: {}", isMacOS);
-        return isMacOS;
     }
 
     private String buildHello() {
         var javaVersion = System.getProperty("java.version");
         var javafxVersion = System.getProperty("javafx.version");
         var javaFxRuntimeVersion = System.getProperty("javafx.runtime.version");
+        log.info("java.version: {}", javaVersion);
         log.info("javafx.version: {}", javafxVersion);
         log.info("javafx.runtime.version: {}", javaFxRuntimeVersion);
         return String.format("Hello, JavaFX  %s running on Java %s",
